9bbe7e957e6dd63aa143299b89add6f5dc683391
bhagyababu1 <bhagyababu1991@gmail.com>
2024-05-14 15:07:52 +0400

    initial commit

--- CVapp/models.py
+++ CVapp/models.py
@@ -8,13 +8,15 @@ class CV(models.Model):
     place=models.CharField(max_length=200)
     mob_no=models.IntegerField()
     email=models.CharField(max_length=200)
-    skills=models.CharField(max_length=5000)
+    skills=models.CharField(max_length=5000,null=True,blank=True)
     full_name=models.CharField(max_length=200)
     date_of_birth=models.DateField()
     sex=models.CharField(max_length=200)
     nationality=models.CharField(max_length=200)
     school_attended=models.CharField(max_length=200)
-    academic_qualifications=models.CharField(max_length=5000)
+    academic_qualifications=models.CharField(max_length=5000,null=True,blank=True)
+    certifications=models.CharField(max_length=5000,null=True,blank=True)
+    work_history=models.CharField(max_length=5000,null=True,blank=True)
 
     def __str__(self):
         return '{}'.format(self.name)
--- CVapp/views.py
+++ CVapp/views.py
@@ -17,8 +17,10 @@ def CreateCV(request):
         nationality = request.POST.get('nationality')
         school_attended = request.POST.get('school_attended')
         academic_qualifications = request.POST.get('academic_qualifications')
+        certifications = request.POST.get('certifications')
+        work_history = request.POST.get('work_history')
 
-        cv = CV(name=name,age=age,place=place,mob_no=mob_no,email=email,skills=skills,full_name=full_name,date_of_birth=date_of_birth,sex=sex,nationality=nationality,school_attended=school_attended,academic_qualifications=academic_qualifications)
+        cv = CV(name=name,age=age,place=place,mob_no=mob_no,email=email,skills=skills,full_name=full_name,date_of_birth=date_of_birth,sex=sex,nationality=nationality,school_attended=school_attended,academic_qualifications=academic_qualifications,certifications=certifications,work_history=work_history)
         cv.save()
         return redirect('/')
     return render(request,'createCv.html')
